98ef97986d68b882c926fe4e7f7d08501134d8ad
Prasen R <prasen@fafadiatech.com>
2015-05-20 18:43:12 +0530

    Added template for listing Links with votes

--- links/views.py
+++ links/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import ListView
+from .models import Link, Vote
+
+class LinkListView(ListView):
+	model = Link
+	queryset = Link.with_votes.all()
+	paginate_by = 5
+
